cac128d3ece6933c1349abb0a08ccd0c773cef17
JoseSuski <51724796+JoseSuski@users.noreply.github.com>
2021-02-20 17:00:18 -0300

    Add files via upload

---
+++ DesafioGoDev/src/Cafe.java
@@ -0,0 +1,20 @@
+public class Cafe {
+
+    String nome;
+
+    public Cafe(String nome) {
+        this.nome = nome;
+    }
+
+    public Cafe() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
